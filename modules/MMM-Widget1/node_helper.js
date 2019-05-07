451f03ed77c7f14748bea913d9ed68d0999b4de0
RakeshKeerthi <sunnykeerthi@gmail.com>
2019-05-07 17:59:42 +0530

    initial commit

---
+++ modules/MMM-Widget1/node_helper.js
@@ -0,0 +1,46 @@
+const bodyParser = require("body-parser")
+
+var NodeHelper = require("node_helper")
+
+module.exports = NodeHelper.create({
+  start: function () {
+    this.config = null
+    this.pooler = []
+  },
+
+  socketNotificationReceived: function (noti, payload) {
+    console.log('pppayload');
+
+    if (noti == "INIT") {
+      console.log(this.config);
+      this.config = payload
+      this.webserver()
+    }
+  },
+
+  webserver: function () {
+    this.expressApp.use(bodyParser.json())
+    this.expressApp.use(bodyParser.urlencoded({ extended: true }))
+    this.expressApp.get("/widget/:id", (req, res) => {
+      var id = req.params.id
+      var html = `
+<html>
+<head>
+<style>
+body {padding:0, margin:0}
+</style>
+</head>
+<body>
+<div>
+`
+        + this.config.widgets[id].html +
+        `
+</div>
+</body>
+</html>`
+      res.status(200).send(this.config.widgets[id].html)
+    })
+
+    this.sendSocketNotification("READY")
+  }
+})
